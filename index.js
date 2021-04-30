2d6b913d025c1ad9150665cf42b32cf49fafaeeb
DesignrKnight <notify.abelmathew@gmail.com>
2021-04-30 20:50:02 +0530

    Init Node Cache

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 const express = require('express');
-const requestIp = require('request-ip');
+const requestIP = require('request-ip');
+const nodeCache = require('node-cache');
+
+const IPCache = new nodeCache({ stdTTL: 10, deleteOnExpire: false, checkperiod: 5 });
 
 const app = express();
 
+IPCache.on('expired', (key, value) => {
+	if (new Date() - value[value.length - 1] > 10000) {
+		IPCache.del(key);
+	}
+	console.log('key', key, 'value', IPCache.get(key));
+});
+
+const updateCache = (ip) => {
+	let cachedIP = IPCache.get(ip);
+	if (!cachedIP) {
+		cachedIP = [];
+	}
+	cachedIP.push(new Date());
+	IPCache.set(ip, cachedIP, (IPCache.getTtl(ip) - Date.now()) / 1000 || 10);
+};
 const ipMiddleware = function (req, res, next) {
-	const clientIp = requestIp.getClientIp(req);
-	console.log(clientIp);
+	const clientIp = requestIP.getClientIp(req);
+	updateCache(clientIp);
+	// IPCache.set(clientIp, new Date(), 30);
+	// const date1 = new Date();
+	// const date2 = new Date('2021-04-30T11:30:25.236Z');
+	// console.log((date2 - date1) / 1000);
+	console.log(IPCache.get(clientIp));
+	console.log('TTL', IPCache.getTtl(clientIp));
 	next();
 };
 
